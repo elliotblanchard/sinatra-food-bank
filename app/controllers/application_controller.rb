5a0781ca5ef56c53dc4d56ca0d1de5dc0a6d7905
Elliot Blanchard <git@elliotblanchard.com>
2020-07-03 00:20:52 -0400

    re-created CLI time hash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 require './config/environment'
+require 'date'
 
 class ApplicationController < Sinatra::Base
 
@@ -21,6 +22,53 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/" do
-    binding.pry
+    #
+    #
+    # CLI find banks code for reference:
+    #  def find_banks
+    #    #Find food banks open at the right time
+    #    same_time = FoodBank::Bank.find_by_time(@time)
+    #    if same_time.length > 0
+    #      # Get distance between the user and every food bank open at the right time / day
+    #      banks_sorted = FoodBank::Mapping.get_distance(@user_address,same_time)
+    #    end
+    #    banks_sorted
+    #  end
+    #
+    #
+    # !!! Need to handle case where DATE and TIME is not supported by browser
+    # !!! Still need to check address - think there's some sort of "certainty" field in the GeoKit response you can look at
+    #
+    #
+    # First task: create the correct time hash from params to send to Bank.find_by_time(time_hash)
+    # You are going to have to have two cases - one for browsers that support DATE / TIME fields, and one for those that don't
+    #
+    #
+    # CLI time hash code:
+    # @time = {}
+    # @days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+    # @time[:day] = input-1
+    # @time[:hour] = time_elements[0]
+    # @time[:minutes] = minutes
+    # @time[:ampm] = ampm
+    #
+    # Example params when using a brower that supports both DATE and TIME fields (like Chrome)
+    #   {"street_number"=>"45", "route"=>"Waverly Avenue", "locality"=>"", 
+    #    "administrative_area_level_1"=>"NY", "postal_code"=>"11205","country"=>"United States",
+    #    "date"=>"2020-07-02","time"=>"12:02"}
+    # Getting the day of week from a Date class object: https://stackoverflow.com/questions/4044574/how-can-i-calculate-the-day-of-the-week-of-a-date-in-ruby
+    # Creating a Date object: https://ruby-doc.org/stdlib-2.6.5/libdoc/date/rdoc/Date.html#method-i-strftime
+    # %w - Day of the week (Sunday is 0, 0..6)
+    #binding.pry
+    time = {}
+    date_elements = params[:date].split("-")
+    time_elements = params[:time].split(":")
+    dateTime = DateTime.new(date_elements[0].to_i,date_elements[1].to_i,date_elements[2].to_i,time_elements[0].to_i,time_elements[1].to_i)
+    time[:day] = dateTime.strftime("%w") #Returns day of week as number starting with Sunday = 0
+    time[:hour] = dateTime.strftime("%l") #Hour of the day, 12 hour format, blank padded as in " 1" instead of "01"
+    time[:minutes] = dateTime.strftime("%M") #Minute of the hour zero padded as in "05"
+    time[:ampm] = dateTime.strftime("%p") #AM/PM
+    #Now that you have the time hash, need to send it to Bank.find_by_time(time_hash)
+
   end
 end
--- app/models/bank.rb
+++ app/models/bank.rb
@@ -1,3 +1,63 @@
 class Bank < ActiveRecord::Base
 
+    def self.find_by_time(time_hash)
+        # Get food banks that are open on the correct day - looks for 'M' as in 'AM' or 'PM' to determine if bank is open
+        correct_day = self.all.select {|a| a.days[time_hash[:day]].include? "M"}
+        
+        # Create a time object from the user requested time
+        user_time = create_time_object(time_hash)
+        
+        # This will be array of food banks that are open at the correct time AND day
+        correct_time = []
+        
+        correct_day.each do |bank| 
+          # Parse the time data in the XML file to extract times
+          time_raw = bank.days[time_hash[:day]].split("-")
+          start_time_raw = time_raw[0].split(":")
+          start_time_raw[0] = start_time_raw[0].scan(/\d/).join('') #only take digits
+          end_time_raw = time_raw[1].split(":")
+          end_time_raw[1] = end_time_raw[1][0,5] #Trim edge case data
+          
+          # Create two hashes using the extracted data
+          bank_time_start_hash = {
+            :day => 0, 
+            :hour => start_time_raw[0].strip.to_i, 
+            :minutes => start_time_raw[1].strip[0,2].to_i, 
+            :ampm => start_time_raw[1].strip[-2..]
+          }
+          
+          bank_time_end_hash = {
+            :day => 0, 
+            :hour => end_time_raw[0].strip.to_i, 
+            :minutes => end_time_raw[1].strip[0,2].to_i, 
+            :ampm => end_time_raw[1][end_time_raw[1].index("M")-1,end_time_raw[1].index("M")]
+          }
+    
+          # Create two time objects from the hashes
+          bank_time_start = create_time_object(bank_time_start_hash)
+          bank_time_end = create_time_object(bank_time_end_hash)
+          
+          #Checking if the user's time is in the open hours of the food bank
+          range = bank_time_start..bank_time_end
+          if range === user_time 
+            correct_time << bank
+          end
+        end
+        correct_time
+      end
+      
+      def self.create_time_object(time_hash)
+        # Quick method to create time objects
+        if time_hash[:ampm] == "AM"
+          time = Time.new(2020, 1, 1, time_hash[:hour], time_hash[:minutes])
+        else
+          if time_hash[:hour].to_i == 12
+            adjusted_hour = 13
+          else 
+            adjusted_hour = time_hash[:hour].to_i+12
+          end
+          time = Time.new(2020, 1, 1, adjusted_hour, time_hash[:minutes])
+        end
+        time
+      end
 end
