c0b90014caf8159a3eedd8c098c0714d125cf0ac
Elliot Blanchard <git@elliotblanchard.com>
2020-07-09 22:50:20 -0400

    Changing for Heroku

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,24 @@
-ENV['SINATRA_ENV'] ||= "development"
-
 require 'bundler/setup'
-Bundler.require(:default, ENV['SINATRA_ENV'])
-
+Bundler.require
+configure :development do
+ ENV['SINATRA_ENV'] ||= "development"
+require 'bundler/setup'
+ Bundler.require(:default, ENV['SINATRA_ENV'])
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+ )
+end
 
-require './app/controllers/application_controller'
-require_all 'app'
+configure :production do
+ db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+
+ ActiveRecord::Base.establish_connection(
+   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+   :host     => db.host,
+   :username => db.user,
+   :password => db.password,
+   :database => db.path[1..-1],
+   :encoding => 'utf8'
+ )
+end
