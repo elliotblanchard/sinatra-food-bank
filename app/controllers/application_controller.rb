749652dd20e4375a71fce5c14e46c4a7e009a9c2
Elliot Blanchard <git@elliotblanchard.com>
2020-07-17 18:11:11 -0400

    Moved date/time calculations out of controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,79 +26,72 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/" do
-    #
-    #
-    # Example params when using a brower that supports both DATE and TIME fields (like Chrome)
-    #   {"street_number"=>"45", "route"=>"Waverly Avenue", "locality"=>"", 
-    #    "administrative_area_level_1"=>"NY", "postal_code"=>"11205","country"=>"United States",
-    #    "date"=>"2020-07-02","time"=>"12:02"}
-    #
-    # Params from safari (no date / time field support)
-    #   {"street_number"=>"311", "route"=>"Wallabout Street", "locality"=>"",
-    #    "administrative_area_level_1"=>"NY", "postal_code"=>"11206", "country"=>"United States",
-    #    "date"=>"8/20/2020", "time"=>"9:20 AM"}
 
-    @time = {}
-    date_elements = Array.new
-    time_elements = Array.new
-
-    #Set up date
-    if params[:date].include? "-"
+    #Date start
+    #....................................
+    #@time = {}
+    #date_elements = Array.new
+    #time_elements = Array.new
+    #
+    #if params[:date].include? "-"
       #Browser supports date field
-      date_elements = params[:date].split("-")
-    elsif (params[:date].include? "/") && (params[:date].split("/").length == 3) 
+    #  date_elements = params[:date].split("-")
+    #elsif (params[:date].include? "/") && (params[:date].split("/").length == 3) 
       #Browser does not support date field 
-      if (params[:date].split("/")[0].to_i > 0) && (params[:date].split("/")[0].to_i < 13) && (params[:date].split("/")[1].to_i > 0) && (params[:date].split("/")[1].to_i < 32) && (params[:date].split("/")[2][-2..-1].to_i > -1) && (params[:date].split("/")[2][-2..-1].to_i < 99)
-        #is valid input
-        temp_elements = params[:date].split("/")
-        date_elements[0] = temp_elements[2]
-        date_elements[1] = temp_elements[0]
-        date_elements[2] = temp_elements[1]  
-      else
+    #  if (params[:date].split("/")[0].to_i > 0) && (params[:date].split("/")[0].to_i < 13) && (params[:date].split("/")[1].to_i > 0) && (params[:date].split("/")[1].to_i < 32) && (params[:date].split("/")[2][-2..-1].to_i > -1) && (params[:date].split("/")[2][-2..-1].to_i < 99)
+    #    #is valid input
+    #    temp_elements = params[:date].split("/")
+    #    date_elements[0] = temp_elements[2]
+    #    date_elements[1] = temp_elements[0]
+    #    date_elements[2] = temp_elements[1]  
+    #  else
         #Default to today
-        date_elements[0] = DateTime.now.in_time_zone('US/Eastern').year
-        date_elements[1] = DateTime.now.in_time_zone('US/Eastern').month
-        date_elements[2] = DateTime.now.in_time_zone('US/Eastern').day 
-      end
-    else
+    #    date_elements[0] = DateTime.now.in_time_zone('US/Eastern').year
+    #    date_elements[1] = DateTime.now.in_time_zone('US/Eastern').month
+    #    date_elements[2] = DateTime.now.in_time_zone('US/Eastern').day 
+    #  end
+    #else
       #Default to today
-      date_elements[0] = DateTime.now.in_time_zone('US/Eastern').year
-      date_elements[1] = DateTime.now.in_time_zone('US/Eastern').month
-      date_elements[2] = DateTime.now.in_time_zone('US/Eastern').day    
-    end
+    #  date_elements[0] = DateTime.now.in_time_zone('US/Eastern').year
+    #  date_elements[1] = DateTime.now.in_time_zone('US/Eastern').month
+    #  date_elements[2] = DateTime.now.in_time_zone('US/Eastern').day    
+    #end
     #Set up time
-    if (!params[:time].include? "M") && (params[:time].split(":").length == 2)
+    #if (!params[:time].include? "M") && (params[:time].split(":").length == 2)
       #Browser supports time field
-      time_elements = params[:time].split(":")
-    elsif (params[:time].split(":").length == 2) && ((params[:time].include? "AM") || (params[:time].include? "PM")) 
+    #  time_elements = params[:time].split(":")
+    #elsif (params[:time].split(":").length == 2) && ((params[:time].include? "AM") || (params[:time].include? "PM")) 
       #Browser does not support time field
-      temp_elements = params[:time].gsub(/[a-zA-Z][a-zA-Z]/, "").split(":") #Remove AM/PM and whitespace before split
-      if params[:time].include? "AM"
-        time_elements[0] = temp_elements[0]
-      else
-        time_elements[0] = temp_elements[0].to_i + 12
-      end
-      time_elements[1] = temp_elements[1]
-    else
+    #  temp_elements = params[:time].gsub(/[a-zA-Z][a-zA-Z]/, "").split(":") #Remove AM/PM and whitespace before split
+    #  if params[:time].include? "AM"
+    #    time_elements[0] = temp_elements[0]
+    #  else
+    #    time_elements[0] = temp_elements[0].to_i + 12
+    #  end
+    #  time_elements[1] = temp_elements[1]
+    #else
       #Default to now
-      time_elements[0] = DateTime.now.in_time_zone('US/Eastern').hour
-      time_elements[1] = DateTime.now.in_time_zone('US/Eastern').min
-    end
+    #  time_elements[0] = DateTime.now.in_time_zone('US/Eastern').hour
+    #  time_elements[1] = DateTime.now.in_time_zone('US/Eastern').min
+    #end
     
     # This catches an invalid date
-    begin
-      dateTime = DateTime.new(date_elements[0].to_i,date_elements[1].to_i,date_elements[2].to_i,time_elements[0].to_i,time_elements[1].to_i)
-    rescue ArgumentError
+    #begin
+    #  dateTime = DateTime.new(date_elements[0].to_i,date_elements[1].to_i,date_elements[2].to_i,time_elements[0].to_i,time_elements[1].to_i)
+    #rescue ArgumentError
       # handle invalid date / time / garbage input
-      dateTime = DateTime.now.in_time_zone('US/Eastern')
-    end
+    #  dateTime = DateTime.now.in_time_zone('US/Eastern')
+    #end
     
-    @time[:day] = dateTime.strftime("%w") #Returns day of week as number starting with Sunday = 0
-    @day_of_week = dateTime.strftime("%A")
-    @time[:hour] = dateTime.strftime("%l") #Hour of the day, 12 hour format, blank padded as in " 1" instead of "01"
-    @time[:minutes] = dateTime.strftime("%M") #Minute of the hour zero padded as in "05"
-    @time[:ampm] = dateTime.strftime("%p") #AM/PM
-    
+    #@time[:day] = dateTime.strftime("%w") #Returns day of week as number starting with Sunday = 0
+    #@day_of_week = dateTime.strftime("%A")
+    #@time[:hour] = dateTime.strftime("%l") #Hour of the day, 12 hour format, blank padded as in " 1" instead of "01"
+    #@time[:minutes] = dateTime.strftime("%M") #Minute of the hour zero padded as in "05"
+    #@time[:ampm] = dateTime.strftime("%p") #AM/PM
+    #....................................  
+    #Date end
+  
+    @time = Bank.create_time_hash(params)
     same_time = Bank.find_by_time(@time)
     
     if same_time.length > 0
@@ -111,23 +104,12 @@ class ApplicationController < Sinatra::Base
         full_address = params[:street_number] + " " + params[:route] + " New York, NY " + params[:postal_code]
       end
 
-      #if params[:street_number] == nil
-      #  params[:street_number] = ""
-      #end
-      #if params[:route] == nil 
-      #  params[:route] = ""
-      #end
-      #if params[:postal_code] == nil 
-      #  params[:postal_code] = ""
-      #end
-
       # Get distance between the user and every food bank open at the right time / day
       @user_location = Mapping.get_location(full_address)
       @banks_sorted = Mapping.get_distance(full_address,same_time,@user_location)
       @api_key =  ENV['GOOGLE_MAPS_API_KEY']
       # Format open times correctly
       @banks_sorted.each do |bank|
-        #days_array = bank.days.gsub(/[^\d\s:,-]/, "").split(',') #Clean out everything except letters, numbers, :, -, comma, and whitespace
         days_array = bank.days.gsub(/"/, "")[1..-2].split(',') #Clean out everything except letters, numbers, :, -, comma, and whitespace
         bank.days = ""
         days_array.each_with_index do |day, index|
@@ -158,7 +140,7 @@ class ApplicationController < Sinatra::Base
     end
 
     #Log search data
-    Log.create(address: full_address, time: dateTime)
+    Log.create(address: full_address, time: DateTime.now.in_time_zone('US/Eastern'))
 
     erb:'/show'
   end
